46dd3a61d39519242ddab03a7b4e7bf9f86a074e
Saint-Jean Germila <germila@noukod.com>
2019-03-19 14:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ function BoardMember(name, homeState, training)
   this.training = training;
 }
 
-  BoardMember.veto = function()
+  BoardMember.prototype.veto = function()
   {
     return 'No, I must disagree'
   };
 
-    BoardMember.approve = function()
+    BoardMember.prototype.approve = function()
   {
     return 'You can do that!'
   };
 
-  BoardMember.doCharity = function()
+  BoardMember.prototype.doCharity = function()
   {
     return "I like to help people."
   };
 
-  BoardMember.releasePressStatement = function()
+  BoardMember.prototype.releasePressStatement = function()
   {
     return "You will see great things from Scuber."
   };
 
 
-  BoardMember.sayHi = function()
+  BoardMember.prototype.sayHi = function()
   {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   };
