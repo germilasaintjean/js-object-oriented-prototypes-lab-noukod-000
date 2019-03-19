ace791e02dfcd25114379b790ce8db3676301014
Saint-Jean Germila <germila@noukod.com>
2019-03-19 14:48:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function BoardMember(name, homeState, training){
 }
 
 BoardMember.prototype.veto = function(){
-    return 'No, I must disagree'
+    console.log{'No, I must disagree'};
   };
 }
 //   this.approve = function(){
