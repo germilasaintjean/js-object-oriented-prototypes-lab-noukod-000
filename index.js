1d33f4a76e11a36bf1413f0c029146b5f25fcb7b
Saint-Jean Germila <germila@noukod.com>
2019-03-19 14:53:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  
+
   BoardMember.veto = function()
   {
     return 'No, I must disagree'
   };
-  
+
     BoardMember.approve = function()
   {
     return 'You can do that!'
   };
-  
+
   BoardMember.doCharity = function()
   {
     return "I like to help people."
@@ -22,7 +22,7 @@ function BoardMember(name, homeState, training){
   {
     return "You will see great things from Scuber."
   };
-  
+
 
     BoardMember.sayHi = function()
   {
