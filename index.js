35c022fe70207eb0ee222570a5ce4aaea3aadf9f
Saint-Jean Germila <germila@noukod.com>
2019-03-19 14:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  BoardMember.veto = function(){
+  
+  BoardMember.veto = function()
+  {
     return 'No, I must disagree'
-
-    BoardMember.approve = function(){
+  };
+  
+    BoardMember.approve = function()
+  {
     return 'You can do that!'
-  }
-  BoardMember.doCharity = function(){
+  };
+  
+  BoardMember.doCharity = function()
+  {
     return "I like to help people."
-  }
+  };
 
-  BoardMember.releasePressStatement = function(){
+  BoardMember.releasePressStatement = function()
+  {
     return "You will see great things from Scuber."
-  }
+  };
+  
 
-    BoardMember.sayHi = function(){
+    BoardMember.sayHi = function()
+  {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
+  };
 }
